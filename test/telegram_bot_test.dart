52d94fc1ec1316618af7b6fcf1d2e87601d70864
Mohamed Usman <emyu10@gmail.com>
2022-11-16 22:34:08 +0530

    write a test test

--- test/telegram_bot_test.dart
+++ test/telegram_bot_test.dart
@@ -1,16 +1,14 @@
 // import 'package:telegram_bot/telegram_bot.dart';
-// import 'package:test/test.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   group('A group of tests', () {
-//     final awesome = Awesome();
-
+void main() {
+  group('A group of tests', () {
 //     setUp(() {
 //       // Additional setup goes here.
 //     });
 
-//     test('First Test', () {
-//       expect(awesome.isAwesome, isTrue);
-//     });
-//   });
-// }
+    test('Test Test', () {
+      expect(1, 2 - 1);
+    });
+  });
+}
